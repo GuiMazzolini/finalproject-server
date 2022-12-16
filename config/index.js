a738ec9d10e644cb19c48634ad29cba82ec371bf
Guilherme Mazzolini <guilhermemazzolini@hotmail.com>
2022-12-16 10:06:47 +0100

    teste

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
   app.use(
     cors({
-      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      origin: "*",
       credentials: true
 
     })
